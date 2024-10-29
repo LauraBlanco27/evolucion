bc4802942be799efec6178c64f46c915668c3533
Laura Blanco <laurablancoc27@gmail.com>
2024-10-29 17:20:45 -0500

    ir a pokemones

--- src/EvolutionPokemonUi.js
+++ src/EvolutionPokemonUi.js
@@ -1,5 +1,8 @@
-import { LitElement, html } from 'lit-element';
-import '@pokemon/pokemon-list-dm/pokemon-list-dm.js';
+import { LitElement, html, css } from 'lit-element';
+import { getComponentSharedStyles } from '@bbva-web-components/bbva-core-lit-helpers';
+import styles from './evolution-pokemon-ui.css.js';
+import '@bbva-experience-components/bbva-button-default/bbva-button-default.js';
+import '@pokemon/pokemon-list-dm/pokemon-list-dm.js'
 
 export class EvolutionPokemonUi extends LitElement {
   static get properties() {
@@ -7,62 +10,81 @@ export class EvolutionPokemonUi extends LitElement {
       pokemonName: { type: String },
       pokemonDetails: { type: Object },
       evolutions: { type: Array },
-      noEvolutionsMessage: { type: String }
+      noEvolutionsMessage: { type: String },
     };
   }
 
-  constructor() {
+  constructor() { 
     super();
-    this.pokemonName = '';
+    this.pokemonName = ''; 
     this.pokemonDetails = {};
     this.evolutions = [];
     this.noEvolutionsMessage = '';
   }
 
   async firstUpdated() {
-    await this._fetchPokemonDetails(this.pokemonName);
+    const PokemonListDm = this.shadowRoot.querySelector('pokemon-list-dm');
+    if (this.pokemonName) {
+      const { pokemonDetails, evolutions } = await PokemonListDm.fetchPokemonDetails(this.pokemonName);
+      this.pokemonDetails = pokemonDetails;
+      this.evolutions = evolutions;
+
+      if (this.evolutions.length === 0) {
+        this.noEvolutionsMessage = 'Este Pokémon no tiene evoluciones.';
+      }
+    }
   }
 
-  async _fetchPokemonDetails(pokemonName) {
-    const PokemonListDm = this.shadowRoot.querySelector('pokemon-list-dm');
-    const { pokemonDetails, evolutions } = await PokemonListDm.fetchPokemonDetailsByName(pokemonName);
+  static get styles() {
+    return [
+      styles,
+      getComponentSharedStyles('evolution-pokemon-ui-shared-styles'),
+    ];
+  }
 
-    this.pokemonDetails = pokemonDetails;
-    this.evolutions = evolutions;
+  async updated(changedProperties) {
+    if (changedProperties.has('pokemonName')) { // Actualización según pokemonName
+      const PokemonListDm = this.shadowRoot.querySelector('pokemon-list-dm');
+      const { pokemonDetails, evolutions } = await PokemonListDm.fetchPokemonDetails(this.pokemonName);
+      this.pokemonDetails = pokemonDetails;
+      this.evolutions = evolutions;
 
-    if (this.evolutions.length === 0) {
-      this.noEvolutionsMessage = 'Este Pokémon no tiene evoluciones.';
+      if (this.evolutions.length === 0) {
+        this.noEvolutionsMessage = 'Este Pokémon no tiene evoluciones.';
+      }
     }
   }
 
   render() {
     return html`
       <div class="pokemon-card">
-        <h1>${this.pokemonDetails.name}</h1>
-        <img src="${this.pokemonDetails.image}" alt="${this.pokemonDetails.name}" class="pokemon-image">
-        <p>Tipos: ${this.pokemonDetails.types}</p>
-        ${this.noEvolutionsMessage 
+        <h1 class="pokemon-name">${this.pokemonDetails.name}</h1>
+        <img src="${this.pokemonDetails.image}" alt="${this.pokemonDetails.name}" class="pokemon-image" />
+        <p><strong>Tipos:</strong> ${this.pokemonDetails.types}</p>
+        
+        ${this.noEvolutionsMessage
           ? html`<p>${this.noEvolutionsMessage}</p>`
           : html`
-            <h2>Evoluciones</h2>
-            ${this.evolutions.map(evolution => html`
-              <div>
-                <img src="${evolution.image}" alt="${evolution.name}">
-                <p>${evolution.name}</p>
+              <div class="evolution-list">
+                <h2>Evoluciones</h2>
+                ${this.evolutions.map(evolution => html`
+                  <div class="evolution-item">
+                    <img src="${evolution.image}" alt="${evolution.name}" class="pokemon-image" />
+                    <p>${evolution.name}</p>
+                  </div>
+                `)}
               </div>
-            `)}
-          `}
+            `}
       </div>
-      <bbva-button-default text="Regresar a Pokémon" @click="${this._goBack}"></bbva-button-default>
+      <div class="button-container">
+        <bbva-button-default @click="${this.gotopokemon}"> Ver pokemones</bbva-button-default>
+      </div>
+
+      <pokemon-list-dm></pokemon-list-dm>
     `;
   }
 
-  _goBack() {
-    this.dispatchEvent(new CustomEvent('navigate-to-pokemon', {
-      bubbles: true,
-      composed: true
-    }));
+  gotopokemon() {
+    this.navigate('pokemon');
   }
 }
-
-window.customElements.define('evolution-pokemon-ui', EvolutionPokemonUi);
